0e97c65e4ae21f77fca3bb10ca8687cf430e47fa
Rodrigo Jeldres C <rodrigo.jeldres@gmail.com>
2024-04-17 21:37:06 -0400

    Actualizada a clase 2024-04-17

--- primera clase.R
+++ primera clase.R
@@ -9,3 +9,24 @@ d<-"1"
 
 pi_value <- pi
 print(pi_value)
+
+x<-1
+print(x)
+
+complejo <- 2+4i
+print(class(complejo))
+
+
+#crear una tabla de datos
+datos<-data.frame(
+  Tarea = c("A", "B", "C", "D"),
+  Puntuacion = c(75, 92, 88, 65)
+)
+attach(datos)
+View(datos$Tarea)
+
+barplot(Puntuacion)
+barplot(Puntuacion, col = "lightblue", main="Puntuacion", xlab = "Tareas", ylab =  "Puntos" , legend.text = row.names(Tarea))
+
+#guardar los datos en un txt
+write.table(datos, file = "datos.txt")
