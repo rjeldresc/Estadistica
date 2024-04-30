1d61d1c5e2e90c4fada04b07e19f00194dcbdef3
Rodrigo Jeldres C <rodrigo.jeldres@gmail.com>
2024-04-29 23:03:27 -0400

    clase 2024-04-29

--- primera clase.R
+++ primera clase.R
@@ -48,3 +48,59 @@ result <- dbGetQuery(con, "SELECT * FROM CampoParametros")
 print(result)
 dbDisconnect(con)
 
+?cor
+
+# Crear un vector de números
+numeros <- c(10, 20, 30, 40, 50)
+
+# Mostrar el vector
+print(numeros)
+
+# Crear un vector de caracteres
+colores <- c("rojo", "verde", "azul")
+
+# Mostrar el vector de caracteres
+print(colores)
+
+# Crear un vector lógico
+booleanos <- c(TRUE, FALSE, TRUE, TRUE, FALSE)
+
+# Mostrar el vector lógico
+print(booleanos)
+
+
+# Crear una lista en R
+mi_lista <- list(
+  nombre = "Juan",
+  edad = 30,
+  ciudad = "Ciudad de México",
+  intereses = c("programación", "estadística", "visualización"),
+  puntajes = c(80, 90, 75, 85)
+)
+
+# Mostrar la lista
+print(mi_lista)
+
+# Crear una matriz en R
+matriz <- matrix(
+  c(1, 2, 3, 4, 5, 6),  # Datos para la matriz
+  nrow = 2,             # Número de filas
+  ncol = 3,             # Número de columnas
+  byrow = TRUE          # Rellenar por filas (TRUE) o por columnas (FALSE)
+)
+
+# Mostrar la matriz
+print(matriz)
+
+# Crear un data frame en R
+data_frame <- data.frame(
+  Nombre = c("Juan", "María", "Carlos"),
+  Edad = c(25, 30, 28),
+  Ciudad = c("Ciudad de México", "Madrid", "Buenos Aires"),
+  Puntaje = c(80, 75, 90)
+)
+
+# Mostrar el data frame
+print(data_frame)
+
+
