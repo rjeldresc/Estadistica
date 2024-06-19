a9ae4253f2af3f6eebd9619da1a9000d213a94dc
Rodrigo Jeldres C <rodrigo.jeldres@gmail.com>
2024-06-19 17:19:45 -0400

    clase 2024-06-03

--- Modelos Continuos.R
+++ Modelos Continuos.R
@@ -22,4 +22,15 @@ lines(x, dlnorm(x, meanlog = 3 , sdlog = 1),
       type = "l" , lwd = 1.5, col = "green" )
 
 
+# Modelo exponencial , es solamente positiva porque depende del tiempo
+x <- seq(from = 0,to = 80,0.001)
+plot(x, dexp(x = x , rate = 1/12), type = "l", lwd = 2 , col = "red")
+lines(x, dexp(x = x , rate = 1/8), type = "l", lwd = 2 , col = "blue")
+lines(x, dexp(x = x , rate = 1/20), type = "l", lwd = 2 , col = "green")
+# rate es el tiempo entre clientes
+# rate = 1/12  , ej llegan 12 clientes por minuto
+
+#modelo chi cuadrado
+# qchisq()
+# dchisq()
 
