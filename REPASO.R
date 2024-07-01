2c6a7b26aa0fae0a67f08c897dd31e9d803325c7
Rodrigo Jeldres C <rodrigo.jeldres@gmail.com>
2024-07-01 17:19:25 -0400

    repaso

--- REPASO.R
+++ REPASO.R
@@ -1,53 +1,71 @@
 ####### REPASO CONTROL 2 #######
-setwd("C:/Users/caroh/Documents/RStudio/Clases/Repaso")
+
+# Paso 1: Desadjuntar todas las instancias de 'base1'
+while("base1" %in% search()) {
+  detach("base1")
+}
+
+# Verificar que ya no hay 'base1' adjuntado
+search()
+
+setwd("d:/dev/Estadistica/")
 
 #------------------------------------
 base1 <- readxl::read_excel("RepasoDIPLO.xlsx", sheet="PCR")
 head(base1)
-attach(base1) 
+attach(base1)
+data <- data.frame(base1)
+data$EXTRANJERO <- as.factor(data$EXTRANJERO)
+
 
 ###PCR
 #Taller_13
 
 #a) ¿Más de 1/3 de los pacientes que concurren son extranjeros residentes?
 # 1. Parametro: P=proporcion de extranjeros
-# 2. H0: P<= 1/3
-#    H1: P>1/3
+# 2. H0: P <= 1/3
+#    H1: P > 1/3
 table(EXTRANJERO)
-prop.test(x=49,n=112,p=1/3,alternative="greater")
+total <- sum(table(EXTRANJERO))
+print(total) # 112
+prop.test( x = 49 , n = 112, p = 1/3, alternative = "greater")
 # p-value = 0.0126 < alpha=5%
 # Se rechaza H0, Hay más de 1/3.
+# Podemos concluir que hay evidencia suficiente para afirmar que más de un 
+# tercio de los pacientes que concurren son extranjeros residentes.
 
 #b) ¿Es posible afirmar que la positividad es inferior a un 30%?
-# 1. Parametro: P=proporcion de positividad
-# 2. H0: P>=0.3
-#    H1: P<0.3
+# 1. Parametro: P = proporcion de positividad
+# 2. H0: P >= 0.3
+#    H1: P < 0.3
 table(PCR)
-prop.test(x=25,n=112,p=0.3,alternative="less")
+total <- sum(table(PCR))
+print(total) # 112
+prop.test( x = 25, n = 112, p = 0.3 , alternative = "less")
 # p-value = 0.04744 < alpha=5%
 # Se rechaza H0. La positividad es inferior a un 30%.
 
 #c)	¿existe evidencia que permita afirmar que los extranjeros que concurren al 
 #   CESFAM son de mayor edad en comparación con los no extranjeros?
-# Parametro: μ_1= edad promedio en no extranjeros 
-#            μ_2= edad promedio en extranjeros 
-# H0:  μ_1 ≥ μ_2    
+# Parametro: μ_1= edad promedio en no extranjeros
+#            μ_2= edad promedio en extranjeros
+# H0:  μ_1 ≥ μ_2
 # H1:  μ_1 < μ_2 (no extranjeros son de menor edad que los extranjeros)
 
 # Parametro:  Var1 = varianza de la edad en no extranjeros
 #              Var2 = varianza de la edad en extranjeros
-# H0: var1 = var2      
+# H0: var1 = var2
 # H1: var1 ≠  var2
-var.test(EDAD~EXTRANJERO,alternative="two.sided")
-# p-value = 0.01362 < alpha=5%
-
-t.test(EDAD~EXTRANJERO,alternative="less",var.equal= FALSE)
+var.test(EDAD ~ EXTRANJERO, alternative = "two.sided" )
+# p-value = 0.01362 < alpha=5% se rechaza h0, entonces var.equal= FALSE
+t.test(data$EDAD ~ data$EXTRANJERO ,data = data, var.equal= FALSE, alternative = "less")
+t.test( EDAD~EXTRANJERO , alternative = "less" , var.equal= FALSE) #var.equal va el resultado anterior
 # p-value = 0.001156 < alpha=5% 
 # Se rechaza H0, extranjeros con de mayor edad que los nos extranjeros
 
 # Estimar las edades promedio (en años)
-Ex <-base1[EXTRANJERO==1,]
-Noex <-base1[EXTRANJERO==0,]
+Ex <- base1[EXTRANJERO==1,"EDAD"] #caso extranjeros
+Noex <- base1[EXTRANJERO==0,"EDAD"] #caso no extranjeros
 
 library(misty)
 ci.mean(Noex$EDAD) #44.02   49.19
@@ -95,22 +113,27 @@ summary(anova)
 #3. Supuestos
 #independencia
 plot(anova$residuals)
+
 #normalidad 
 #H0: Residuos distribuyen normal
 shapiro.test(anova$residuals) #Se usa para tamaños de muestras menor a 50 datos
+#p-value = 0.1021
+
 #homocedasticidad (varianzas iguales)
 #H0: varianza iguales
 library(lmtest)
 bptest(anova)
+#p-value = 0.2272
 
 #4. Comparaciones
-TukeyHSD(anova) 
+TukeyHSD(anova)
 plot(TukeyHSD(anova), las=1)
 #Entre los centros C y A hay diferencias en las medias de los diametros y es menor en el C.
+#es menor en centro C
 
 #------------------------------------
 base3 <- readxl::read_excel("RepasoDIPLO.xlsx", sheet="Marketing")
-attach(base3) 
+attach(base3)
 
 ### MARKETING
 #Taller_12
@@ -122,7 +145,7 @@ par(mfrow = c(1,3))
 hist(Youtube,freq=FALSE)
 hist(Facebook,freq=FALSE)
 hist(Periodico,freq=FALSE)
-par(mfrow = c(1,1)) 
+par(mfrow = c(1,1))
 
 par(mfrow = c(1,3))
 boxplot(Youtube, main="Youtube", horizontal=TRUE)
@@ -161,7 +184,7 @@ t.test(menores$Periodico,mayores$Periodico, alternative="less",var.equal = TRUE)
 # c) ¿La inversión publicitaria en Periódico distribuye Gamma? 
 # (indicación: utilice estimador de EMV y test KS para un nivel de significancia del 10%)
 library(fitdistrplus)
-gam <-fitdist(Periodico, "gamma",method="mle")
+gam <- fitdist(Periodico, "gamma", method="mle")
 parametro1 <- gam$estimate[1]   
 parametro2 <- gam$estimate[2]
 
