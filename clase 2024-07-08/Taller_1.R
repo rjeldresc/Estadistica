4c4051bbfcb56ee25c4aedea46ad6ec8a97dfebd
Rodrigo Jeldres C <rodrigo.jeldres@gmail.com>
2024-07-22 21:12:26 -0400

    actualizada clase 2024-07-10

--- clase 2024-07-08/Taller_1.R
+++ clase 2024-07-08/Taller_1.R
@@ -22,7 +22,7 @@ pairs(datos,upper.panel= panel.smooth, lower.panel = panel.smooth)  #lower.panel
 correlacion <- cor(datos) #por defecto es pearson (method='pearson'), otros valores posibles son kendall y spearman.
 # r = 1 -> perfecta asociación lineal
 # r < 0 -> correlación negativa, si una aumenta la otra disminuye
-
+print(correlacion)
 # Matriz de correlacion
 library(corrplot)
 corrplot(correlacion, type="lower")
@@ -42,12 +42,12 @@ corrplot(correlacion, type="lower", method = "number")
 #3. Inferencia
 #4. Análisis de la varianza
 #5. Coeficiente de determinación
-#6. Análisis de supuestos: linealidad, homocedasticidad, independencia, norrmalidad
+#6. Análisis de supuestos: linealidad, homocedasticidad, independencia, normalidad
 
 
 #1.Especificación del modelo 
 # El modelo que se va a ajustar es: 
-# Resistencia = B0 + B1*Edad
+# Resistencia = B0 + B1 * Edad
 # Supuestos: - los residuos distribuyen normal
 #            - los residuos son independientes
 #            - los residuos tienen varianza constante (Homocedasticidad)
@@ -58,7 +58,7 @@ mod <- lm(Resistencia ~ Edad, data=datos)
 # Resistencia ~ Edad indica que Resistencia es la variable respuesta y que Edad es la variable explicativa
 summary(mod)
 
-# Resistencia = 2596.856 -33.556*Edad
+# Resistencia = 2596.856 -33.556 * Edad
 
 # - Por cada semana que envejezca la soldadura, se espera que la resistencia promedio disminuya en 33.556 psi.
 # - Si la soldadura es nueva (Edad=0), se espera que la resistencia promedio sea de 2596.856 psi.
@@ -69,22 +69,26 @@ confint(mod)
 # Incluyamos la recta de regresión que representa el modelo ajustado anterior
 ggplot(datos, aes(x=Edad, y=Resistencia)) + 
   geom_point() +
-  geom_smooth(method='lm', formula=y~x, se=TRUE, col='lightblue') 
+  geom_smooth(method='lm', formula=y~x, se=TRUE, col='red') 
 # opción se=FALSE o TRUE, muestra el intervalo de confianza de la regresión.
 
 
-#3. Inferencia
-#H0: B1  = 0 
-#H1: B1 ≠ 0
+#3. Inferencia (hipotesis para validar si la variable edad es significativa )
+# H0: B1  = 0 
+# H1: B1 ≠ 0
 summary(mod)
-# Estadístico T = -9.334 con valor p = 1.58e-08 < alpha=5%, se rechazo H0, la variable Edad (X) si explica Resistencia (Y) 
+# Estadístico T = -9.334 con valor p = 1.58e-08 < alpha=5%, 
+#se rechazo H0, la variable Edad (X) sí explica Resistencia (Y) 
 
 
 #4. Análisis de la varianza
-# A cada coeficiente del modelo de Regresión Lineal Simple se le puede asociar un test. 
+# A cada coeficiente del modelo de Regresión Lineal Simple 
+# se le puede asociar un test. 
+
 # H0: No existe regresión 
 # H1: existe regresión
 anova <- aov(mod)
+summary(mod)
 summary(anova)
 # valor p=1.58e-08 < alpha = 5%, se rechaza H0, existe regresión.
 par(mfrow = c(2, 2))
@@ -107,7 +111,8 @@ par(mfrow = c(1, 1))
 
 
 #5. Coeficientes de determinación
-# El coeficiente de determinación, es el cuadrado del coeficiente de correlación de Pearson y
+# El coeficiente de determinación, es el cuadrado del coeficiente de correlación 
+# de Pearson y
 # mide la bondad del ajuste de la recta a los datos.
 summary(mod)
 # Multiple R-squared:  0.821,	Adjusted R-squared:  0.8115 
@@ -125,6 +130,7 @@ plot(mod, 2)
 ks.test(mod$residuals, "pnorm", mean(mod$residuals), sd(mod$residuals))
 nortest::lillie.test(mod$residuals)
 # p-value = 0.09597 > alpha = 5%, no se rechaza H0. Los residuos son normales
+shapiro.test(mod$residuals)
 
 # Homocedasticidad  
 # H0: Homocedasticidad
@@ -132,33 +138,38 @@ nortest::lillie.test(mod$residuals)
 lmtest::bptest(mod)
 # p-value = 0.1723
 
-# Independencia 
+# Independencia (en casos temporales)
 # H0: No hay autocorrelacion
 # H1: hay autocorrelacion
 lmtest::dwtest(mod)
 # p-value = 0.3061. Si hay independencia
 
 
-           ########### Anomalías
+########### Anomalías ########
 library(car)
 outlierTest(mod)
 summary(influence.measures(mod))
+# h0 es que no hay anomalia , h0 es hipotesis de no cambio
 
 #Podemos sacar el datos número 21 y realizamos el modelo nuevamente.
-datos2 <- datos[1:20,] 
+datos2 <- datos[1:20,]
+datos2 <- datos[-21, ]
+
 mod2 <- lm(Resistencia ~ Edad, datos2)
 summary(mod2)
-# modelo 1: Resistencia = 2596.856 -33.556*Edad 
-# modelo 2: Resistencia = 2627.822 -37.154*Edad
+# modelo 1: Resistencia = 2596.856 - 33.556 * Edad # con los 21 registros
+# R-squared:  0.821
+# modelo 2: Resistencia = 2627.822 - 37.154 * Edad # sin el registro 21
+# R-squared:  0.9018 tiene mejor porcentaje aunque se debe invertigar si eliminar o no el registro
 
 g_mod1<- ggplot(datos, aes(x=Edad, y=Resistencia)) + 
   geom_point() +
-  geom_smooth(method='lm', formula=y~x, se=TRUE, col='lightblue') +
+  geom_smooth(method='lm', formula=y~x, se=TRUE, col='darkblue') +
   labs(title = "Modelo 1")
 
 g_mod2<- ggplot(datos2, aes(x=Edad, y=Resistencia)) + 
   geom_point() +
-  geom_smooth(method='lm', formula=y~x, se=TRUE, col='lightblue') +
+  geom_smooth(method='lm', formula=y~x, se=TRUE, col='darkblue') +
   labs(title = "Modelo 2")
 
 library(gridExtra)
@@ -179,4 +190,8 @@ ggplot(datos2, aes(x=Edad, y=Resistencia)) +
   geom_point() +
   geom_point(aes(y=predicciones), col='red')
 
-# los punto negros son los datos originales, los rojos son las estimaciones, los residuales son las lineas de color rojo
+# los punto negros son los datos originales, los rojos son las estimaciones, 
+# los residuales son las lineas de color rojo
+
+
+
