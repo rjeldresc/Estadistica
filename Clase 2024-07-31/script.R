b039ce157ee3e2d8b7cf4463dcec5a660c592a94
Rodrigo Jeldres C <rodrigo.jeldres@gmail.com>
2024-08-16 21:32:55 -0400

    repaso clase 2024-07-31

--- Clase 2024-07-31/script.R
+++ Clase 2024-07-31/script.R
@@ -1,3 +1,4 @@
+install.packages("faraway")
 library(faraway) #contiene los datos
 
 ## visualizar los datos
@@ -5,19 +6,25 @@ faraway::wcgs
 data(wcgs)
 head(wcgs)
 ?faraway::wcgs
-  
+
+#variable respuesta = chd
+#covariable = cigs
+
 ## analisis descriptivo
-boxplot(wcgs$cigs ~ wcgs$chd )
+boxplot(wcgs$cigs ~ wcgs$chd)
 tapply(wcgs$cigs , wcgs$chd , mean) #media por grupo
-tapply(wcgs$cigs , wcgs$chd , sd)   #sd por grupo
+tapply(wcgs$cigs , wcgs$chd , sd)   #desviacion estandar por grupo
 
 var.test(wcgs$cigs ~ wcgs$chd)
+
 #prueba t para validar si las medias son iguales o distintas
 t.test(wcgs$cigs ~ wcgs$chd , var.equal = F ) #prueba de diferencia de medias
-#se rechaza que las medias son iguales
+#se rechaza que las medias son iguales t = -5.4472, df = 295.31, p-value = 1.079e-07
 
 ## modelo regresion logistica
-reglog1 <- glm(chd ~ cigs , data = wcgs , family = binomial(link = "logit"))
+#chd es categorica
+?glm
+reglog1 <- glm(chd ~ cigs , data = wcgs , family = binomial(link = "logit")) #chd depende de la cantidad de cigs
 reglog1
 
 summary(reglog1)
@@ -26,38 +33,39 @@ summary(reglog1)
 reglog1$coefficients
 exp(reglog1$coefficients)
 
-## odd para x=1 exp(b0 + b1*X)
+## odd para x=1 exp(b0 + b1*X) x es la cantidad de cigarrillos, en este caso 1
 sum(reglog1$coefficients)
 o1 = exp(sum(reglog1$coefficients))
-o1
+o1 #0.06594464
 
-## odd para x=0 exp(b0 + b1*X)
+## odd para x=0 exp(b0 + b1*X) caso no fuma cigarrillos
 sum(reglog1$coefficients[1])
 o0 = exp(sum(reglog1$coefficients[1]))
-o0
+o0 #0.06443106
 
 ## odd ratio x = 1 vs x = 0
-o1/o0 #1.023491
+o1/o0 # 1.023491
 exp(sum(reglog1$coefficients[2]))
 
 
 ## odd ratio x = 10 vs x = 0
-exp(sum(reglog1$coefficients[2] * 10)) 
+exp(sum(reglog1$coefficients[2] * 10))
 #resp :  1.26137
 
-#calcular la probabilidad de tener un infarto si x = 10
-exp(reglog1$coefficients[1] + reglog1$coefficients[2]  * 10 ) /
+#Calcular la probabilidad de tener un infarto si x = 10
+       exp(reglog1$coefficients[1] + reglog1$coefficients[2] * 10 ) /
   (1 + exp(reglog1$coefficients[1] + reglog1$coefficients[2] * 10))
 #0.07516279
 
-newdata <- data.frame( cigs = 0:200)
-pred.prod1 <- predict(reglog1, newdata, type = "response" )
+#para predecir
+newdata <- data.frame(cigs = 0:200)
+pred.prod1 <- predict(reglog1, newdata, type = "response" ) #calcula la probabilidad por cada x , por cada cigarrillo
 predict(reglog1, newdata, type = "link" ) #funcion enlace calcula xbeta
 
 plot(newdata$cigs , pred.prod1 , type = "l")
+abline(h=0.5)
 
-#segundo modelo
-
+# Segundo modelo
 reglog2 <- glm(chd ~ cigs + age , data = wcgs , family = binomial(link = "logit"))
 reglog2
 summary(reglog2)
