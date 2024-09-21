4baa697680bea93fa9b8ec162acb3ba1b47d2521
Rodrigo Jeldres C <rodrigo.jeldres@gmail.com>
2024-09-20 21:04:20 -0300

    actualizado control series de tiempo

--- Series de Tiempo/Control/Control.R
+++ Series de Tiempo/Control/Control.R
@@ -77,23 +77,26 @@ mod_diff <- forecast::Arima(Y, order = c(2, 1, 2), seasonal = c(1, 1, 1))  # d=1
 plot(Y)
 lines(mod_diff$fitted, col = "orange")
 
-# Definir regresores (ejemplo con dummy y tiempo, puedes agregar PIB, IMACEC, etc.)
+# Definir regresores (ejemplo con dummy y tiempo, agregar PIB, IMACEC, etc.)
 xreg <- as.matrix(data.frame(tiempo = 1:length(Y), dummy = rep(0, length(Y))))
 xreg[1:24,2] <- 1  # Dummy para primeros 24 meses como ejemplo
 
 # Modelo SARIMAX con componentes SARIMA (p,d,q) y regresores
 mod_sarimax <- forecast::auto.arima(Y, xreg = xreg, seasonal = TRUE, lambda = "auto")
+LSTS::ts.diag(c(mod_sarimax$residuals), 36)
 
 # Graficar ajuste
 plot(Y, main = "Demanda Real de Energía (GWh) y Ajuste SARIMAX")
 lines(mod_sarimax$fitted, col = "red")
 
-
 # Test de homocedasticidad
 lmtest::bptest(lm(mod_sarimax$residuals ~ time(mod_sarimax$residuals)))
-
+pacf(mod_sarimax$residuals,12)
 source("TS.diag.R")
 TS.diag(c(mod_sarimax$res))
+
+#resumen del modelo
+summary(mod_sarimax)
 
 # Generar valores futuros de los regresores
 # Suponiendo que el regresor 'tiempo' continúa linealmente y que el dummy sigue siendo 0 después del periodo inicial.
@@ -104,6 +107,8 @@ future_xreg <- as.matrix(data.frame(
 
 # Realizar el pronóstico con los nuevos valores de xreg
 new_pre <- forecast::forecast(mod_sarimax, h = 36, level = 0.95, xreg = future_xreg)
+LSTS::ts.diag(c(new_pre$residuals), 36)
+summary(new_pre)
 par(mfrow = c(1,1))
 # Graficar el pronóstico
 plot(new_pre)
@@ -117,11 +122,14 @@ mean(abs(new_pre$mean/Z-1))*100
 pred_2024 <- sum(new_pre$mean[1:12])
 pred_2025 <- sum(new_pre$mean[13:24])
 pred_2026 <- sum(new_pre$mean[25:36])
-
+pred_2024
+pred_2025
+pred_2026
 # Calcular tasas de crecimiento anuales
 tasa_2025 <- (pred_2025 - pred_2024) / pred_2024 * 100
 tasa_2026 <- (pred_2026 - pred_2025) / pred_2025 * 100
-
+tasa_2025
+tasa_2026
 
 ###### Caso con regresor IMACEC
 
@@ -137,17 +145,18 @@ mod_sarimax <- auto.arima(Y, xreg = X, seasonal = TRUE, lambda = "auto")
 
 # Resumen del modelo ajustado
 summary(mod_sarimax)
-
+TS.diag(c(mod_sarimax$res))
 # Graficar la serie original y el ajuste
 plot(Y, main = "Demanda Real de Energía (GWh) y Ajuste SARIMAX con IMACEC")
 lines(fitted(mod_sarimax), col = "red")
 
 # Supongamos que el IMACEC futuro sigue igual a la media histórica de los últimos 12 meses
-future_imacec <- rep(mean(Imacec_filtered$IMACEC), 12)
+future_imacec <- rep(mean(Imacec_filtered$IMACEC), 36)
 
 # Si tienes proyecciones del IMACEC, puedes usar esas en lugar de la media
 # future_imacec <- c(valor_proyectado_1, valor_proyectado_2, ... , valor_proyectado_12)
-
+# Test de homocedasticidad
+lmtest::bptest(lm(mod_sarimax$residuals ~ time(mod_sarimax$residuals)))
 # Generar la predicción para los próximos 36 meses
 predicciones <- forecast(mod_sarimax, xreg = future_imacec, h = 36)
 
